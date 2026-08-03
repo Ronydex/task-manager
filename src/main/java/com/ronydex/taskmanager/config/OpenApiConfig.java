0209b58dbedf03e892296cd2b27c624ed383c76c
Ronydex <ronygaleana3103@gmail.com>
2026-08-02 22:19:28 -0600

    feat:Agregado Documentacion con Swagger API

---
+++ src/main/java/com/ronydex/taskmanager/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.ronydex.taskmanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+			.info(new Info()
+				.title("API REST - Task Manager")
+				.version("1.0.0")
+				.description("Documentacion interactiva de la API para la gestión de tareas y asignación de usuarios. "));
+	}
+	
+}
