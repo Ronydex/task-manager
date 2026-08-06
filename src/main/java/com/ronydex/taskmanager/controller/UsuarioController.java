712762071cfc3d1fd3b323ffe2db8812ecc13cf6
Ronydex <ronygaleana3103@gmail.com>
2026-08-05 20:56:47 -0600

    feat:Documentacion en Swagger para los Controladores

--- src/main/java/com/ronydex/taskmanager/controller/UsuarioController.java
+++ src/main/java/com/ronydex/taskmanager/controller/UsuarioController.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
 import com.ronydex.taskmanager.dto.UsuarioResponseDTO;
 import com.ronydex.taskmanager.dto.UsuarioRegistroDTO;
 import com.ronydex.taskmanager.service.UsuarioService;
@@ -16,6 +24,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@Tag(name = "Usuarios", description = "Endpoints para la gestión de los usuarios")
 public class UsuarioController {
 
 	private final UsuarioService usuarioServ;
@@ -23,14 +32,24 @@ public class UsuarioController {
 	public UsuarioController(UsuarioService usuarioServ){
 	this.usuarioServ = usuarioServ;
 	}
-
+	
+	@Operation(summary = "Crear un usuario", description = "Permite crear un usuario de acuerdo a las caracteristicas del Model")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "Usuario creado de manera exitosa"),
+		@ApiResponse(responseCode = "400", description = "Fallo al crear el usuario")
+	})
 	@PostMapping
 	public ResponseEntity<UsuarioResponseDTO> crearUsuario(@Valid @RequestBody UsuarioRegistroDTO userRegDTO){
 		UsuarioResponseDTO usuarioResp = usuarioServ.registrarUsuario(userRegDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioResp);
 		
 	}
-	
+
+	@Operation(summary = "Devolver un usuario por Id", description = "Permite devolver el usuario solicitado por medio de la Id")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "Usuario devuelto de manera exitosa por el Id"),
+		@ApiResponse(responseCode = "400", description = "Error al encontrar al usuario por medio del Id")
+	})	
 	@GetMapping("/{id}")
 	public ResponseEntity<UsuarioResponseDTO> devolverUsuario(@PathVariable Long id){
 		UsuarioResponseDTO usuarioResp = usuarioServ.obtenerPorId(id);
