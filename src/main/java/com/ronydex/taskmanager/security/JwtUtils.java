9773fae285a7556d5b5e0077b8d26fdbd7dbbb0f
Ronydex <ronygaleana3103@gmail.com>
2026-08-08 15:52:26 -0600

    feat:Implementacion de JwtUtils

--- src/main/java/com/ronydex/taskmanager/security/JwtUtils.java
+++ src/main/java/com/ronydex/taskmanager/security/JwtUtils.java
@@ -2,3 +2,19 @@ package com.ronydex.taskmanager.security;
 
 @Component
 
+public class JwtUtils{
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
+	@Value("{jwt.expiration}")
+	private int jwtExpirationMs;
+
+	public String generateToken(UsuarioPrincipal username){
+		Date fechaAhora = new Date();
+		Date fechaExpiracion = new Date(fechaAhora.getTime() + jwtExpirationMs);
+		Jwts.builder
+	}
+
+	public JwtUtil
+
+}
