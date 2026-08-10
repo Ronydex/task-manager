cda114f4f657aff448e182a941e423bc0fbb8b9b
Ronydex <ronygaleana3103@gmail.com>
2026-08-09 21:28:38 -0600

    feat:Implementación de Seguridad por medio de JwtAuthenticationFilter para autenticar usuarios

---
+++ src/main/java/com/ronydex/taskmanager/security/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.ronydex.taskmanager.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+@Component
+public class JwtAuthenticatioFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	public JwtUtils jwtUtils;
+
+	@Autowired
+	public UserDetailsService userDetailsServ;
+	
+	@Override
+	protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException{
+		String authHeader = request.getHeader("Authorization");
+		
+			if(authHeader != null && authHeader.startsWith("Bearer ")){
+				String token = authHeader.substring(7);
+					if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+					UserDetails userDetails = userDetailsService.loadByUsername(username);
+					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+							userDetails,
+							null,
+							userDetails.getAuthorities()
+							);
+					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext.setAuthentication(authToken);
+					
+			}
+		}
+	}
+	filterChain.doFilter(request, response);
+}
