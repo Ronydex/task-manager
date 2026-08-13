c83e70cf4d41a97d24444911585e170869e9aa46
Ronydex <ronygaleana3103@gmail.com>
2026-08-12 21:59:17 -0600

    feat:Implementación de SecurityConfig con control de autenticaciones del usuario al ingresar

--- src/main/java/com/ronydex/taskmanager/security/SecurityConfig.java
+++ src/main/java/com/ronydex/taskmanager/security/SecurityConfig.java
@@ -1,1 +1,53 @@
-package com.ronydex.taskmanager
+package com.ronydex.taskmanager.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Bean
+	private SecurityFilterChain securityFilterChain(HttpSecurity http){
+		http
+			//Paso 1.Apagar CSRF porque se usaran tokens JWT(stateless)
+			.csrf(csrf -> csrf.disable())
+			//Paso 2.Definir que NO guarde sesiones en memoria del servidor
+			.sessionManagement(session ->
+					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+					)
+			//Paso 3.Reglas de acceso a las URLs
+			.authorizedHttpRequests(auth -> auth
+				//Endpoints públicos (login, registro)
+				.requestMatchers("/api/auth/**").permitAll()
+				//Cualquier otra URL exige estar autenticado
+				.anyRequest().authenticated()
+				)
+			//Paso 4.Poner tu filtro JWT antes del filtro por defecto de Spring
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+			//Paso 5.Construir y devolver la configuración armada
+			return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+}
